7fbcfd8abb450e97283a09045d65ccec587c70cc
Bastian Bresson <bastian@bresson.dk>
2020-07-10 16:15:56 +0200

    Fixed missing references

--- Assets/Scripts/ObjectPooling/PoolObjectSpawner.cs
+++ Assets/Scripts/ObjectPooling/PoolObjectSpawner.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CuberSpawnerFromPool : MonoBehaviour
+public class PoolObjectSpawner : MonoBehaviour
 {
     public GameObject spawnBox;
     public float spawnFrequency;
