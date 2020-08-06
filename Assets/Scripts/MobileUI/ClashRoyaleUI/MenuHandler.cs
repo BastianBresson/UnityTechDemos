8d9ed450236ed356fb4c1b65c997946369c164eb
Bastian Bresson <bastianlol@live.dk>
2020-08-06 23:18:34 +0200

    Added social page and top- and bottom-bar animations

---
+++ Assets/Scripts/MobileUI/ClashRoyaleUI/MenuHandler.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI.Extensions;
+
+public class MenuHandler : MonoBehaviour
+{
+    [SerializeField] private HorizontalScrollSnap pageScroller = default;
+    [SerializeField] private List<PageHandler> pages = default;
+
+    [SerializeField] private List<GameObject> menuButtons = default;
+
+    private int currentPage;
+
+    private float normaltWidth;
+
+    bool isInitialised;
+
+    private void Start()
+    {
+        pageScroller.OnSelectionPageChangedEvent.AddListener(OnPageChanged);
+
+        StartCoroutine(InitilizeButtons());
+    }
+
+
+    public void ChangePage(int pageNumber)
+    {
+        if (currentPage == pageNumber) return;
+
+        pageScroller.ChangePage(pageNumber);
+    }
+
+    public void OnPageChanged(int pageNumber)
+    {
+        if (!isInitialised) return;
+        Debug.Log($"Page {pageNumber} is now the active page");
+        currentPage = pageNumber;
+        TweenButtons();
+
+        AlertPage(pageNumber);
+    }
+
+    private void AlertPage(int pageNumber)
+    {
+        pages[pageNumber].OnBecomeActive();
+    }
+
+    private void TweenButtons()
+    {
+        for (int i = 0; i < menuButtons.Count; i++)
+        {
+            RectTransform rect = menuButtons[i].GetComponent<RectTransform>();
+
+            if (i == currentPage)
+            {
+                TweenBig(rect);
+            }
+            else
+            {
+                TweenSmall(rect);
+            }
+        }
+    }
+
+    private void TweenBig(RectTransform rect)
+    {
+        Vector2 size = rect.rect.size;
+
+        size.x = normaltWidth + 100;
+
+        LeanTween.size(rect, size, 0.2f).setEaseInOutSine();
+    }
+
+    private void TweenSmall(RectTransform rect)
+    {
+        Vector2 size = rect.rect.size;
+
+        size.x = normaltWidth - 50;
+        LeanTween.size(rect, size, 0.2f).setEaseInOutSine();
+    }
+
+    private IEnumerator InitilizeButtons()
+    {
+        yield return new WaitForEndOfFrame();
+
+        normaltWidth = menuButtons[0].GetComponent<RectTransform>().rect.size.x;
+
+        currentPage = pageScroller.CurrentPage;
+
+        isInitialised = true;
+
+        OnPageChanged(currentPage);
+    }
+}
---
+++ Assets/Scripts/MobileUI/ClashRoyaleUI/PageHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageHandler : MonoBehaviour
+{
+    [SerializeField] private bool showTopBar = default;
+
+    [SerializeField] private GameObject topBar = default;
+
+    private static bool isTopBarActive = true;
+
+    public void OnBecomeActive()
+    {
+        if (!showTopBar)
+        {
+            MakeTopBarDisappear();
+        }
+        else if (!isTopBarActive)
+        {
+            MakeTopBarAppear();
+        }
+    }
+
+    private void MakeTopBarAppear()
+    {
+        topBar.SetActive(true);
+        isTopBarActive = true;
+        LeanTween.scaleY(topBar, 1, 0.2f).setEaseOutSine();
+    }
+
+    private void MakeTopBarDisappear()
+    {
+        isTopBarActive = false;
+        LeanTween.scaleY(topBar, 0, 0.2f).setEaseOutSine();
+        Invoke(nameof(DisableTopBar), 0.2f);
+    }
+
+    private void EnableTopBar()
+    {
+        topBar.SetActive(true);
+    }
+
+    private void DisableTopBar()
+    {
+        topBar.SetActive(false);
+    }
+}
